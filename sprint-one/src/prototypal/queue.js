9f916f9efe74abc5bd6571c39ea0d81d8e5727c5
chaseme3 <chaseme3@gmail.com>
2014-10-30 11:12:53 -0700

    completed proto

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,43 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.length = 0;
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.length + 1] = value;
+    this.length++;
+  },
 
+  dequeue: function(){
+    //save value of front of line
+    var numToReturn = this.storage[1];
+    //delete value at front of line
+    delete this.storage[1];
+
+    if(this.length > 0) {
+      this.length--;
+    };
+    //loop through keys and update keys
+    for (var key in this.storage) {
+      key = key - 1;
+    }
+    //return saved value
+    return numToReturn;
+  },
+
+  size: function () {
+    return this.length;
+  }
+};
+
+var newStack = makeQueue();
+newStack.size;
+newStack.enqueue("a");
+console.log(newStack.length);
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,31 +1,32 @@
 var makeStack = function() {
-  var obj = Object.create(makeStack.stackMethods);
+  var obj = Object.create(stackMethods);
   obj.storage = {};
   obj.length = 0;
 
   return obj;
 };
 
-makeStack.stackMethods = {};
+var stackMethods = {
+      push: function(value){
+        this.storage[this.length + 1] = value;
+        this.length++;
+      },
 
-makeStack.stackMethods.enqueue = function(value){
-  this.storage[this.length + 1] = value;
-  this.length++;
-};
+      pop: function(){
+        var popValue = this.storage[this.length];
+        delete this.storage[this.length];
+        if (this.length>0) {
+        this.length--;
+        };
+        return popValue;
+      },
 
-makeStack.stackMethods.dequeue = function(){
-  var numToReturn = this.storage[1];
-  delete this.storage[1];
-  if(this.length>0){
-    this.length--;
-  }
-  for(var key in this.storage){
-    key = key -1;
-  }
-  return numToReturn;
-};
+      size: function(){
+        return this.length;
+      }
+}
 
-makeStack.stackMethods.size = function(){
-  return this.length;
-};
-
+var newStack = makeStack();
+newStack.push("a");
+newStack.push("b");
+console.log(newStack.length);
