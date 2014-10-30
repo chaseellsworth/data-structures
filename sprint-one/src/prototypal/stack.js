0fb917ad6697dadab729088acc1786182703ece0
chaseme3 <chaseme3@gmail.com>
2014-10-29 23:04:38 -0700

    updated prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(makeStack.stackMethods);
+  obj.storage = {};
+  obj.length = 0;
+
+  return obj;
 };
 
-var stackMethods = {};
+makeStack.stackMethods = {};
 
+makeStack.stackMethods.enqueue = function(value){
+  this.storage[this.length + 1] = value;
+  this.length++;
+};
+
+makeStack.stackMethods.dequeue = function(){
+  var numToReturn = this.storage[1];
+  delete this.storage[1];
+  if(this.length>0){
+    this.length--;
+  }
+  for(var key in this.storage){
+    key = key -1;
+  }
+  return numToReturn;
+};
+
+makeStack.stackMethods.size = function(){
+  return this.length;
+};
 
